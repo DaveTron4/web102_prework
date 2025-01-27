34ce2e7a1733b0626a7e3f7ddc0ef6aedbdb2a39
DaveTron4 <davidtnt970@gmail.com>
2025-01-27 17:37:26 -0500

    made a small fix

--- server.js
+++ server.js
@@ -13,7 +13,6 @@ app.use(express.static('public'));
 app.get("/api/games", async (req, res) => {
     const gameName = req.query.search;
     const apiKey = process.env.API_KEY;  // Get the API key from the .env file
-    console.log(apiKey);
     try {
         const response = await axios.get(`https://api.rawg.io/api/games?key=${apiKey}&search=${gameName}`);
         res.json(response.data);  // Send the API data as a response
